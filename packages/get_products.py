58f3fb1c33c1352dfba50f496f37800f2da9c533
Alfredo Sangiuliano <agsangiuliano@gmail.com>
2024-07-29 16:21:48 -0300

    test3

--- packages/get_products.py
+++ packages/get_products.py
@@ -17,7 +17,6 @@ def list_s3_buckets():
         print(f"Error listing buckets: {e}")
     return buckets
 
-
 def list_lambda_functions():
     # Create a Lambda client
     lambdaF = {}
@@ -27,7 +26,7 @@ def list_lambda_functions():
         # Call list_functions to get all Lambda functions
         response = lambda_client.list_functions()
 
-        # Print information about each function
+        # Adding information about each function to lambdaF dict
         for function in response['Functions']:
             lambdaF[function['FunctionName']] = function['LastModified']
     except boto3.exceptions.Boto3Error as e:
@@ -43,17 +42,49 @@ def list_sagemaker_domains():
         # Call list_domains to get all domains
         response = sagemaker_client.list_domains()
 
-        # Print information about each domain
-        print("SageMaker Domains:")
+        # Adding information about each domain to sagemakerdomains dict
         for domain in response['Domains']:
             sagemakerdomains[domain['DomainName']] = domain['CreationTime']
     except boto3.exceptions.Boto3Error as e:
         print(f"Error listing SageMaker domains: {e}")
     return sagemakerdomains
 
+def list_sagemaker_images():
+    # Create a SageMaker client
+    sagemaker_client = boto3.client('sagemaker')
+
+    try:
+        # Call list_images to get all SageMaker images
+        response = sagemaker_client.list_images()
+        sagemakerimages = {}
+
+        # Adding information about each imageto 
+        print("SageMaker Images:")
+        for image in response['Images']:
+            sagemakerimages[image['ImageName']] = image['CreationTime']
+    except boto3.exceptions.Boto3Error as e:
+        print(f"Error listing SageMaker images: {e}")
+    return sagemakerimages
+
+def list_ec2_snapshots():
+    # Crear un cliente para EC2
+    ec2_client = boto3.client('ec2')
+    ec2_snapshots = {}
+    
+    try:
+        # Call describe_snapshots to get all snapshots
+        response = ec2_client.describe_snapshots(OwnerIds=['self'])
+        
+        # Mostrar información sobre cada snapshot
+        for snapshot in response['Snapshots']:
+            ec2_snapshots[snapshot['Description']] = snapshot['StartTime']
+    except boto3.exceptions.Boto3Error as e:
+        print(f"Error al listar los snapshots: {e}")
 
 pps['Buckets'] = list_s3_buckets()
 pps['Lambda'] = list_lambda_functions()
 pps['SageMakerDomains'] = list_sagemaker_domains()
+pps['SageMakerImages'] = list_sagemaker_images()
+pps['EC2_snapshots'] = list_sagemaker_images()
 
 pprint(pps)
