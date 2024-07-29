967775e66f59331af100fb4b8eba6d506f695026
Alfredo Sangiuliano <agsangiuliano@gmail.com>
2024-07-29 15:30:48 -0300

    Adding lambda functions to get_products.py

--- packages/get_products.py
+++ packages/get_products.py
@@ -5,19 +5,35 @@ from pprint import pprint
 pps = {}
 
 def list_s3_buckets():
-    # Create s3 client instance
+    counter = 0
     s3_client = boto3.client('s3')
 
     try:
-        # Llamar a list_buckets para obtener todos los buckets
+        # Call list_cuckets to get buckets: 
         response = s3_client.list_buckets()
-
-        # Mostrar los nombres de los buckets
-        # print("Buckets en tu cuenta de S3:")
         for bucket in response['Buckets']:
-            pps['s3'] = [{bucket['Name']}]
-
+            counter += 1
+            pps[f'Bucket {str(counter)}'] = bucket['Name']
     except boto3.exceptions.Boto3Error as e:
-        print(f"Error al listar los buckets: {e}")
+        print(f"Error listing buckets: {e}")
+    return pps
+
+
+def list_lambda_functions():
+    # Create a Lambda client
+    counter = 0
+    lambda_client = boto3.client('lambda')
+
+    try:
+        # Call list_functions to get all Lambda functions
+        response = lambda_client.list_functions()
+
+        # Print information about each function
+        for function in response['Functions']:
+            counter += 1
+            pps[f'Lambda Function {str(counter)}'] = function['FunctionName']
+    except boto3.exceptions.Boto3Error as e:
+        print(f"Error listing Lambda functions: {e}")
+
 
 pprint(list_s3_buckets())
